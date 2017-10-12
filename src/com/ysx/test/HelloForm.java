39c7ded3f97127f868e1c054ef2ce323b8ce9ded
develop_yang <develop_yang@yeah.net>
2017-10-12 18:10:44 +0800

    add ReadCookies
    Signed-off-by: develop_yang <develop_yang@yeah.net>

--- src/com/ysx/test/HelloForm.java
+++ src/com/ysx/test/HelloForm.java
@@ -2,9 +2,11 @@ package com.ysx.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,12 +32,25 @@ public class HelloForm extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        // 为名字和姓氏创建 Cookie
+        Cookie nameCookie = new Cookie("name", URLEncoder.encode(request.getParameter("name"), "UTF-8")); // 中文转码
+        Cookie urlCookie = new Cookie("url", request.getParameter("url"));
+
+        // 为两个 Cookie 设置过期日期为 24 小时后
+        nameCookie.setMaxAge(60 * 60 * 24);
+        urlCookie.setMaxAge(60 * 60 * 24);
+
+        // 在响应头中添加两个 Cookie
+        response.addCookie(nameCookie);
+        response.addCookie(urlCookie);
+
         // 设置响应内容类型
         response.setContentType("text/html;charset=UTF-8");
-//        response.setCharacterEncoding("utf-8");
+        // response.setCharacterEncoding("utf-8");
 
         PrintWriter out = response.getWriter();
-        
+
         String title = "使用 GET 方法读取表单数据";
         // 处理中文
         String name = new String(request.getParameter("name").getBytes("ISO8859-1"), "UTF-8");
